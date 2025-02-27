0f18efa062c26c473d79a302dee3302660f1b81b
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-27 13:00:58 +0530

    Update queue.py

--- SONALI_MUSIC/utils/inline/queue.py
+++ SONALI_MUSIC/utils/inline/queue.py
@@ -66,20 +66,19 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        [   
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users",)
         ],
         [
-            InlineKeyboardButton(
-                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/ONE_WAS_SIGMA"
-            ),
-            InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/+1NPyVr47HYtiYWE9"
-            ),
+            InlineKeyboardButton(text="˹ ʀєsᴜᴍє ˼", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="˹ ᴘᴧᴜsє ˼", callback_data=f"ADMIN Pause|{chat_id}"),
         ],
-      ]
+        [
+            InlineKeyboardButton(text="˹ sᴋɪᴘ ˼", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="˹ sᴛσᴘ ˼", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close",),
+        ]
+    ]
     return buttons
