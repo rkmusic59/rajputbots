b0455213a074f2e7b7bf4a808964110b88502575
rkmusic59 <rkmusic5911@gmail.com>
2025-04-07 17:08:52 +0530

    Update quiz.py

--- SONALI_MUSIC/Plugins/tools/quiz.py
+++ SONALI_MUSIC/Plugins/tools/quiz.py
@@ -5,7 +5,7 @@ import html
 from pyrogram import filters
 from pyrogram.enums import PollType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Grabber import app  # Replace with actual import
+from SONALI_MUSIC import app  # Replace with actual import
 
 quiz_loops = {}
 active_polls = {}
